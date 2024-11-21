3e411d0a60e6df8a91cdd670a954d9432f25dd2e
JojoELITE <jojoelite1014@gmail.com>
2024-11-21 12:39:18 +0100

    code

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,12 @@
-export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+import type { NextRequest } from 'next/server';
  
-export function GET(request: Request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+export function GET(request: NextRequest) {
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
+ 
+  return Response.json({ success: true });
 }
